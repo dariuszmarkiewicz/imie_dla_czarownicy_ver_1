7590bc2beee9480ca1031be42e7835476d9633c9
Dariusz Markiewicz <97259830+dariuszmarkiewicz@users.noreply.github.com>
2022-07-12 12:30:25 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string imie, pseudonim;
+    cin >> imie >> pseudonim;
+
+    int ascii_pierwszej_imienia = int(imie[0]);
+
+    for(int i=0; i<pseudonim.size(); i++)
+    {
+        if(int(pseudonim[i])==ascii_pierwszej_imienia)
+        {
+            if(pseudonim.size()-i>=imie.size())
+            {
+                cout << "TAK" << endl;
+                break;
+            }
+
+        else
+                cout << "NIE" << endl;
+        }
+    }
+
+    return 0;
+}
