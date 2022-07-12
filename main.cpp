4bf2796fe9528fa57d866b8f53374fbeaa9e8092
Dariusz Markiewicz <97259830+dariuszmarkiewicz@users.noreply.github.com>
2022-07-12 13:07:44 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main()
     string imie, pseudonim;
     cin >> imie >> pseudonim;
 
-    int ascii_pierwszej_imienia = int(imie[0]);
+    //int ascii_pierwszej_imienia = int(imie[0]);
 
     for(int i=0; i<pseudonim.size(); i++)
     {
-        if(int(pseudonim[i])==ascii_pierwszej_imienia)
+        if(int(pseudonim[i])==int(imie[0]))
         {
             if(pseudonim.size()-i>=imie.size())
             {
